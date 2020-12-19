7c7e12cb0af4d7bbdd593910b99c1713ad8693da
Carlo Capocasa <carlo@capocasa.net>
2020-12-19 10:37:09 +0100

    visibility and api

--- dewebm.nim
+++ dewebm.nim
@@ -33,8 +33,10 @@ cPlugin:
 # supplement automatic conversions with hand-edits
 cOverride:
   const
-    CODEC_UNKNOWN = high(cint)
-    TRACK_UNKNOWN = high(cint)
+    CODEC_UNKNOWN* = high(cint)
+    TRACK_UNKNOWN* = high(cint)
+  #[
+  # keep this override around in case we need to downgrade nimterop
   type
     io {.importc: "nestegg_io", header: headernestegg, bycopy} = object
       read: proc(buffer: pointer, length: csize_t, userdata: pointer): cint {.cdecl}
@@ -42,13 +44,16 @@ cOverride:
       tell: proc(userdata: pointer): clonglong {.cdecl}
       userdata: pointer
     log = proc(context: ptr nestegg, severity: cuint, format: cstring) {.cdecl}
+  ]#
 
 # import symbols
 cImport nesteggPath, recurse=false
 
+
 # compose higher-level API
 
 proc log_callback(context: ptr nestegg, severity: cuint, format: cstring) {.cdecl} =
+  # TODO: implement logging
   discard
 
 const unknownValue = high(int8)
@@ -67,35 +72,42 @@ type
     tkVideo = (TRACK_VIDEO, "video")
     tkAudio = (TRACK_AUDIO, "audio")
     tkUnknown = (unknownValue, "unknown")
-  TrackObj* = object
-    case kind: TrackKind
-    of tkVideo:
-      videoCodec: VideoCodec
-      videoParams: video_params
-    of tkAudio:
-      audioCodec: AudioCodec
-      audioParams: audio_params
-    of tkUnknown:
-      discard
-  Track* = ref TrackObj
+
   InitError* = object of IOError
   DemuxError* = object of IOError
-  DemuxerObj* = object
-    file: File
-    context: ptr nestegg
-    duration: uint64
-    codecdata, ptrvar: ptr cuchar
-    io: io
-    tracks: seq[Track]
-  Demuxer = ref DemuxerObj
-  PacketObj* = object
-    raw: ptr cpacket
-    track: Track
-    n: cuint
-    timestamp: culonglong
-  Packet = ref PacketObj
 
-proc file_read(buffer: pointer, length: csize_t, file: pointer): cint {.cdecl} =
+  TrackObj* = object
+    case kind*: TrackKind
+    of tkVideo:
+      videoCodec*: VideoCodec
+      videoParams*: video_params
+    of tkAudio:
+      audioCodec*: AudioCodec
+      audioParams*: audio_params
+    of tkUnknown:
+      discard
+    num*: csize_t
+  Track* = ref TrackObj
+  ChunkObj* = object
+    size*: csize_t
+    data*: ptr UncheckedArray[byte]
+  Chunk* = ref ChunkObj
+  PacketObj* = object
+    raw*: ptr cpacket
+    length*: cuint
+    timestamp*: culonglong
+    track*: Track
+  Packet* = ref PacketObj
+  DemuxerObj* = object
+    file*: File
+    context*: ptr nestegg
+    duration*: uint64
+    codecdata*, ptrvar*: ptr cuchar
+    io*: io
+    tracks*: seq[Track]
+  Demuxer* = ref DemuxerObj
+
+proc file_read*(buffer: pointer, length: csize_t, file: pointer): cint {.cdecl} =
   let file = cast[File](file)
   let n = file.readBuffer(buffer, length)
   if n == 0:
@@ -105,19 +117,19 @@ proc file_read(buffer: pointer, length: csize_t, file: pointer): cint {.cdecl} =
       return -1
   return 1
 
-proc file_seek(offset: clonglong, whence: cint, file: pointer): cint {.cdecl} =
+proc file_seek*(offset: clonglong, whence: cint, file: pointer): cint {.cdecl} =
   let file = cast[File](file)
   file.setFilePos(offset, whence.FileSeekPos)
 
-proc file_tell(file: pointer): clonglong {.cdecl} =
+proc file_tell*(file: pointer): clonglong {.cdecl} =
   let file = cast[File](file)
   return file.getFilePos
 
 proc cleanup(track: Track) =
   discard
 
-proc newTrack(context: ptr nestegg, i: cuint): Track =
-  let trackType = track_type(context, i)
+proc newTrack*(context: ptr nestegg, trackNum: cuint): Track =
+  let trackType = track_type(context, trackNum)
   let kind = case trackType:
     of TRACK_UNKNOWN:
       tkUnknown
@@ -131,13 +143,14 @@ proc newTrack(context: ptr nestegg, i: cuint): Track =
     result = Track(kind: kind)
   case result.kind:
   of tkVideo:
-    if 0 != track_video_params(context, i, result.videoParams.addr):
-      raise newException(InitError, "error initializing video track metadata $#" % $i)
+    if 0 != track_video_params(context, trackNum, result.videoParams.addr):
+      raise newException(InitError, "error initializing video track metadata $#" % $trackNum)
   of tkAudio:
-    if 0 != track_audio_params(context, i, result.audioParams.addr):
-      raise newException(InitError, "error initializing audio track metadata $#" % $i)
+    if 0 != track_audio_params(context, trackNum, result.audioParams.addr):
+      raise newException(InitError, "error initializing audio track metadata $#" % $trackNum)
   else:
     discard
+  result.num = trackNum
 
 proc cleanup(demuxer: Demuxer) =
   destroy(demuxer.context)
@@ -159,8 +172,9 @@ proc newDemuxer*(file: File): Demuxer =
   if 0 != duration(result.context, result.duration.addr):
       raise newException(InitError, "could not retrieve duration")
 
+  result.tracks.setLen(n)
   for i in 0..<n:
-    result.tracks.add(newTrack(result.context, i))
+    result.tracks[i] = newTrack(result.context, i)
 
 proc cleanup(packet: Packet) =
   free_packet(packet.raw)
@@ -173,19 +187,20 @@ iterator packets*(demuxer: Demuxer): Packet =
     if 0 != packet_track(packet.raw, i.addr):
       raise newException(DemuxError, "could not retrieve packet track number")
     packet.track = demuxer.tracks[i]
-    if 0 != packet_count(packet.raw, packet.n.addr):
+    if 0 != packet_count(packet.raw, packet.length.addr):
       raise newException(DemuxError, "could not retrieve number of data objects")
     if 0 != packet_tstamp(packet.raw, packet.timestamp.addr):
       raise newException(DemuxError, "could not retrieve packet timestamp")
     yield packet
 
-iterator data*(packet: Packet): (csize_t, ptr UncheckedArray[byte]) =
-  for i in 0..<packet.n:
-    var chunk:ptr cuchar
-    var size:csize_t
-    if 0 != packet_data(packet.raw, i.cuint, chunk.addr, size.addr):
+iterator data*(packet: Packet): Chunk =
+  for i in 0..<packet.length:
+    var chunk:Chunk
+    new(chunk)
+    if 0 != packet_data(packet.raw, i.cuint, cast[ptr ptr cuchar](chunk.data.addr), chunk.size.addr):
       raise newException(DemuxError, "could not retrieve data chunk")
-    yield (size, cast[ptr UncheckedArray[byte]](chunk))
+    yield chunk
 
-
+template toOpenArray*(chunk:Chunk, first, last: int): openArray[byte] =
+  toOpenArray(chunk.data, first, last)
 
---
+++ dewebm.nimble
@@ -0,0 +1,11 @@
+# Package
+
+version       = "0.1.0"
+author        = "Carlo Capocasa"
+description   = "dewebm, a demuxer for the webm audio/video container format, which is a subset of the matroska container format. Wraps the portable nestecc C library linking statically."
+license       = "0BSD"
+
+# Dependencies
+
+requires "nim >= 1.4.0"
+requires "nimterop"
